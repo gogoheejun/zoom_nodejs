78dbea5bd930dcc0f1aa5adf94afdd226b2e60c3
heejun <heejjuunn@gmail.com>
2021-08-17 17:55:50 +0900

    socket.io연결

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import http from "http";
-import WebSocket from "ws";
+import SocketIO from "socket.io";
 import express from "express";
 
 const app = express();
@@ -9,33 +9,32 @@ app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
 
-const handleListen = () => console.log("listening to http://localhost");
+const httpServer = http.createServer(app);
+const wsServer = SocketIO(httpServer); //독자적으로 만들 수 있지만, 3000포트에 http랑 동시에 서버를 열고 싶어서 이렇게 함
 
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server }); //독자적으로 만들 수 있지만, 3000포트에 http랑 동시에 서버를 열고 싶어서 이렇게 함
-
-function onSocketClose() {
-  console.log("Disconnected from the Broweser!");
-}
-
-const sockets = [];
-
-wss.on("connection", (socket) => {
-  sockets.push(socket);
-  socket["nickname"] = "익명";
-  console.log("connected to brower!");
-  socket.on("close", onSocketClose);
-  socket.on("message", (msg) => {
-    const message = JSON.parse(msg); //string으로 전달된 json을 Json으로 변환
-    switch (message.type) {
-      case "new_message":
-        sockets.forEach((aSocket) =>
-          aSocket.send(`${socket.nickname}: ${message.payload}`)
-        );
-      case "nickname":
-        socket["nickname"] = message.payload; //소켓에다가 새로운 속성을 넣은 것임
-    }
-  });
+wsServer.on("connection", (socket) => {
+  console.log(socket);
 });
 
-server.listen(3000, handleListen);
+// const sockets = [];
+
+// wss.on("connection", (socket) => {
+//   sockets.push(socket);
+//   socket["nickname"] = "익명";
+//   console.log("connected to brower!");
+//   socket.on("close", onSocketClose);
+//   socket.on("message", (msg) => {
+//     const message = JSON.parse(msg); //string으로 전달된 json을 Json으로 변환
+//     switch (message.type) {
+//       case "new_message":
+//         sockets.forEach((aSocket) =>
+//           aSocket.send(`${socket.nickname}: ${message.payload}`)
+//         );
+//       case "nickname":
+//         socket["nickname"] = message.payload; //소켓에다가 새로운 속성을 넣은 것임
+//     }
+//   });
+// });
+
+const handleListen = () => console.log("listening to http://localhost");
+httpServer.listen(3000, handleListen);
